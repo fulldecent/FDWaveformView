396f41b47303483e7d55bb08d7377f5ecda636bc
William Entriken <github.com@phor.net>
2015-03-16 18:42:37 -0400

    Update tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -8,20 +8,6 @@
 
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
-
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
-    
-    it(@"will wait and fail", ^AsyncBlock {
-        
-    });
-});
 
 describe(@"these will pass", ^{
     
@@ -31,12 +17,6 @@ describe(@"these will pass", ^{
     
     it(@"can read", ^{
         expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^AsyncBlock {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            done();
-        });
     });
 });
 
--- FDWaveformView.podspec
+++ FDWaveformView.podspec
@@ -22,7 +22,7 @@ Pod::Spec.new do |s|
   s.source           = { :git => "https://github.com/fulldecent/FDWaveformView.git", :tag => s.version.to_s }
   s.social_media_url = 'https://twitter.com/fulldecent'
 
-  s.platform     = :ios, '7.0'
+  s.platform     = :ios, '7.1'
   s.requires_arc = true
 
   s.source_files = 'Pod/Classes/**/*'
