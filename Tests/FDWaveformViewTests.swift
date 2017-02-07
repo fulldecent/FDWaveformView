978d09b92cdf8435f2ad2877e98395deaecc42c2
Kip Nicol <ospr@me.com>
2017-02-07 09:06:55 -0800

    Clean up tests.

--- Tests/FDWaveformViewTests.swift
+++ Tests/FDWaveformViewTests.swift
@@ -16,24 +16,14 @@ class FDWaveformViewTests: XCTestCase {
     override func setUp() {
         super.setUp()
         waveformView = FDWaveformView()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
-    func testExample() {
-        XCTAssert(true)
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testFD() {
-        XCTAssert(waveformView != nil)
-    }
-
+    // MARK: - Tests
+    
     func testZoomSaples() {
         XCTAssert(waveformView.zoomStartSamples == 0)
         XCTAssert(waveformView.zoomEndSamples == 0)
